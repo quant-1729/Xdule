8c319aeeba5cf75b3b596092b6611b1e9c386270
quant-1729 <priyanshuagrawal1729@gmail.com>
2023-03-14 13:59:52 +0530

    This is my atv commit

--- app/src/main/java/com/example/xdule/MainActivity.java
+++ app/src/main/java/com/example/xdule/MainActivity.java
@@ -3,14 +3,22 @@ package com.example.xdule;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
     private WebView webView;
     private Button button;
+    private Toolbar toolbar;
+    private AutoCompleteTextView autoCompleteTextView;
     String url="https://www.mnnit.ac.in/#login_form";
+    String [] atvtext= {"Monday", "Tuesday", "Wednesday","Thursday", "Friday", "Saturday", "Sunday"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +31,17 @@ public class MainActivity extends AppCompatActivity {
         webview1.setWebChromeClient(new WebChromeClient());
         webview1.loadUrl(url);
 
+        //removing the action bar
+        // the app is crashing on adding the supportrequestwindowsfeature;
+        //supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+        //removing the action bar completed
+
+
+        //Setting auto complete days
+        AutoCompleteTextView atv1= findViewById(R.id.atv1);
+        atv1.setAdapter(new ArrayAdapter<>(MainActivity.this,android.R.layout.simple_list_item_1, atvtext));
 
 
 
@@ -30,4 +49,12 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+
+
+
+
+
+
+
 }
